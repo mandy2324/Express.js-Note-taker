132bd935d5979861217d2e68fceaa44bb21c20ed
unknown <jasdeepsaini30@gmail.com>
2021-11-04 01:47:25 -0400

    adding all the formatted code

---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// const api = require('./routes/routes.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(express.static(__dirname));
+
+//Require routes file
+require('./routes/routes')(app);
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} `)
+);
